84b5f6e3b02026229ab61e4aff09a1e89b885e1b
rz <569225144@qq.com>
2019-02-15 19:10:08 +0800

    home-头部

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,31 +1,122 @@
 <template>
-
-        <el-container class="container">
-            <el-row>
-                <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-                <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-                <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            </el-row>
+        <el-container class="container" >
+            <!-- 顶部 -->
+            <el-header>
+                <el-row>
+                    <el-col :span="4">
+                        <div class="grid-content bg-purple">
+                            <img src="../assets/logo.png" alt="">
+                        </div></el-col>
+                    <el-col :span="19" class="maddle">
+                        <div class="grid-content bg-purple-light">
+                            <h2>电商后台管理系统</h2>
+                        </div>
+                    </el-col>
+                    <el-col :span="1">
+                        <div class="grid-content bg-purple">
+                            <a href="#" class="loginout" @click.prevent="handleLoginout()">退出</a>
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-header>
         <el-container>
-         <el-aside class="aside" width="200px">Aside</el-aside>
-         <el-main class="main">Main</el-main>
+            <el-aside class="aside" width="200px">
+             <!-- 导航 -->
+                <el-menu default-active="1" :router="true" :unique-opened="true">
+                    <!-- 用户管理 -->
+                    <el-submenu index="1">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                        </template>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>用户列表</el-menu-item>
+                    </el-submenu>
+                    <!-- 权限管理 -->
+                    <el-submenu index="2">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>
+                        </template>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>角色列表</el-menu-item>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>权限列表</el-menu-item>
+                    </el-submenu>
+                    <!-- 商品 -->
+                    <el-submenu index="3">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>
+                        </template>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品列表</el-menu-item>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>分类列表</el-menu-item>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>商品分类</el-menu-item>
+                    </el-submenu>
+                    <!-- 订单 -->
+                    <el-submenu index="4">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>
+                        </template>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>订单列表</el-menu-item>
+                    </el-submenu>
+                    <!-- 数据统计 -->
+                    <el-submenu index="5">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>数据统计</span>
+                        </template>
+                        <el-menu-item index="1-1"><i class="el-icon-menu"></i>数据报表</el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-main class="main">Main</el-main>
         </el-container>
-</el-container>
+    </el-container>
 </template>
 
 <script>
-export default {}
+export default {
+// 付过用户没有登录，/标识匹配是，不能进入home。vue -》显示login。vue
+// if(有没有正确的token){渲染}
+// if（没有token）{改login ， 显示login}
+  beforeMount () {
+    console.log('beforeMount-----')
+    if (localStorage.getItem('token')) {
+      this.$router.push({
+        name: 'home'
+      })
+    }
+  },
+  methods: {
+      handleLoginout(){
+            // 1，清楚token、
+        localStorage.clear();
+        this.$router.push({
+            name:'login'
+        });
+        this.$message.warn('退出登录')
+    }
+  },
+}
 </script>
 
 <style>
-.container {
-  height: 100%;
-  background: yellow;
+.container{
+    height: 100%;
+    background: #b3c0d1;
 }
-.aside {
-  background: red;
+.aside{
+     /* background: red; */
 }
-.main {
-  background: rgb(0, 255, 55);
+.main{
+    background: gray;
+}
+.maddle{
+    line-height: 60px;
+    text-align: center;
+}
+.loginout{
+    text-decoration: none;
+    line-height: 60px;
+    text-align: center;
 }
 </style>
